91c4464afb37a3b4500842722ad2f5821b49bb5f
WGLabz <oksbwn@gmail.com>
2020-03-21 19:09:26 +0530

    Outgoing call detection working !!

--- Code/app/src/main/java/in/wglabz/alberto/main/helpers/CallHelper.java
+++ Code/app/src/main/java/in/wglabz/alberto/main/helpers/CallHelper.java
@@ -1,14 +1,8 @@
 package in.wglabz.alberto.main.helpers;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.util.Log;
-import android.widget.Toast;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -18,37 +12,23 @@ public class CallHelper {
     private Context con;
     private TelephonyManager telephonyManager;
     private CallStateListener callStateListener;
-    private OutgoingReceiver outgoingReceiver;
-    private boolean outputCallDetected;
     private long callStartTime;
     private long callEndTime;
 
     public CallHelper(Context ctx) {
         this.con = ctx;
         callStateListener = new CallStateListener();
-        outgoingReceiver = new OutgoingReceiver();
     }
-
 
     public void start() {
         telephonyManager = (TelephonyManager) con.getSystemService(Context.TELEPHONY_SERVICE);
         telephonyManager.listen(callStateListener, PhoneStateListener.LISTEN_CALL_STATE);
-
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_NEW_OUTGOING_CALL);
-        con.registerReceiver(outgoingReceiver, intentFilter);
     }
 
-    /**
-     * Stop calls detection.
-     */
     public void stop() {
         telephonyManager.listen(callStateListener, PhoneStateListener.LISTEN_NONE);
-        con.unregisterReceiver(outgoingReceiver);
     }
 
-    /**
-     * Listener to detect incoming calls.
-     */
     private class CallStateListener extends PhoneStateListener {
 
         @Override
@@ -84,7 +64,7 @@ public class CallHelper {
                         MqttHelper.publishMessage("bikash/call",jsonData.toString());
 
                     }
-                    if (outputCallDetected) {
+                  else {
                         callEndTime = System.currentTimeMillis();
                         callEndTime = (callEndTime - callStartTime) / 1000;
                         details_.put("DURATION",callEndTime);
@@ -96,28 +76,8 @@ public class CallHelper {
                     }
                     incomingCallReceived = false;
                     incomingCallDetected = false;
-                    outputCallDetected = false;
-
                     break;
             }
         }
     }
-
-    /**
-     * Broadcast receiver to detect the outgoing calls.
-     */
-    public class OutgoingReceiver extends BroadcastReceiver {
-
-        public OutgoingReceiver() {
-            Log.w("CALL_RECEIVER","OUTGOING CALL REGISTERD");
-        }
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.w("CALL_RECEIVER","Out Call Detected !!");
-            outputCallDetected = true;
-            callStartTime = System.currentTimeMillis();
-        }
-    }
-
 }
